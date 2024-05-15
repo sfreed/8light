33b2b822e58dea43be709fc85279240b1ef0f2e3
Shane Freed <shane.freed@gmail.com>
2024-05-15 11:57:11 -0400

    initial commit

---
+++ 8light/src/main/java/com/srfreed/eightlight/CodingChallenge.java
@@ -0,0 +1,5 @@
+package com.srfreed.eightlight;
+
+public class CodingChallenge {
+
+}
---
+++ 8light/src/test/java/com/srfreed/eightlight/CodingChallengeTest.java
@@ -0,0 +1,14 @@
+package com.srfreed.eightlight;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CodingChallengeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
